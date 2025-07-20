1a490c635ae365721319af9fcc0d95813099ac8b
emilywengster <62787480+emilywengster@users.noreply.github.com>
2025-07-20 10:32:29 +0800

    Add files via upload

---
+++ CS100/minesweeper_stu.py
@@ -0,0 +1,67 @@
+# Minesweeper Starter Template (Student Version)
+
+import random
+
+def create_board(size, num_bombs):
+    # TODO: Create a size x size board filled with ' '
+    # TODO: Randomly place '*' bombs on the board (num_bombs of them)
+    pass
+
+def count_bombs(board, row, col):
+    # TODO: Count the number of bombs around a given cell (8 neighbors)
+    pass
+
+def add_numbers(board):
+    # TODO: For each cell that is not a bomb, count surrounding bombs and fill it in
+    pass
+
+def create_hidden_board(size):
+    # TODO: Create a board of same size with all cells as '■' (or other hidden marker)
+    pass
+
+def print_board(board):
+    # TODO: Print the board in a user-friendly format with row/col labels
+    pass
+
+def reveal_cell(board, hidden_board, row, col):
+    # TODO: Reveal the cell on the hidden board
+    # If it's a bomb, return False (game over). Else, update and return True.
+    pass
+
+def play_game(size=5, num_bombs=3):
+    board = create_board(size, num_bombs)
+    add_numbers(board)
+    hidden = create_hidden_board(size)
+    revealed = 0
+    total_safe = size * size - num_bombs
+
+    while True:
+        print_board(hidden)
+        try:
+            row = int(input("Enter row: "))
+            col = int(input("Enter col: "))
+        except ValueError:
+            print("Invalid input. Try again.")
+            continue
+
+        if not (0 <= row < size and 0 <= col < size):
+            print("Out of range. Try again.")
+            continue
+
+        if hidden[row][col] != '■':
+            print("Cell already revealed. Choose another.")
+            continue
+
+        if not reveal_cell(board, hidden, row, col):
+            print_board(board)
+            print("💥 You hit a bomb! Game over.")
+            break
+
+        revealed += 1
+        if revealed == total_safe:
+            print_board(hidden)
+            print("🎉 You win!")
+            break
+
+# Uncomment this line when you're ready to test your game!
+# play_game()
